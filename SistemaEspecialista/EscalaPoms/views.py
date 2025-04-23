883a73ff51a20ee7dba03a2e1c366840a4e1a826
gusstavomotta <127642611+gusstavomotta@users.noreply.github.com>
2025-04-23 15:06:37 -0300

    Update views.py

--- SistemaEspecialista/EscalaPoms/views.py
+++ SistemaEspecialista/EscalaPoms/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 # Create your views here.
 
@@ -15,3 +15,14 @@ def listar_personal(request):
 def lista_escalas(request):
     escalas = EscalaPoms.objects.all()
     return render(request, 'EscalaPoms/escalas.html', {'escalas': escalas})
+
+#Exemplo de view que recebe um POST do HTML e salva no banco de dados, depois ela redireciona para a lista de alunos ou retorna para o formulário original
+#Não está sendo usando no momento
+def adicionar_aluno(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')  
+        novo_aluno = Aluno(nome=nome)    
+        novo_aluno.save()                
+        return redirect('listar_alunos') 
+    else:
+        return render(request, 'adicionar_aluno.html') 
