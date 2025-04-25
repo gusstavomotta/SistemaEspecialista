3b5db92aae7a76d35276a98a4b98e90631b76d5a
gusstavomotta <127642611+gusstavomotta@users.noreply.github.com>
2025-04-25 01:20:48 -0300

    Atualizado 01:20
    URL de login foi alterada de /login para /

--- SistemaEspecialista/EscalaPoms/urls.py
+++ SistemaEspecialista/EscalaPoms/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 #Aqui definimos as urls que serão utilizadas no projeto e associamos a uma função que será chamada quando a url for acessada.
 urlpatterns = [
-    path('login/', login, name='login'),
+    path('', login, name='login'),
     path('cadastro/', cadastro, name='cadastro'),
     path('dashboard/', dashboard, name='dashboard'),
     path('escala/', escala, name='escala'),
