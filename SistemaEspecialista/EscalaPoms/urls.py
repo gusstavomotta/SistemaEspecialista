e6637bb61062afd392bdef52b4f8818bc2d66595
Gustavo Oliveira da Motta <127642611+gusstavomotta@users.noreply.github.com>
2025-05-19 16:12:49 -0300

    Atualizado 16:15

--- SistemaEspecialista/EscalaPoms/urls.py
+++ SistemaEspecialista/EscalaPoms/urls.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+from django.urls import path, re_path
+from django.views.static import serve
 
 from .views import *
 
@@ -46,3 +48,14 @@ urlpatterns = [
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+# **garante** mídia em qualquer DEBUG
+urlpatterns += [
+    re_path(
+        r'^media/(?P<path>.*)$',
+        serve,
+        {'document_root': settings.MEDIA_ROOT},
+        name='media'
+    ),
+]
